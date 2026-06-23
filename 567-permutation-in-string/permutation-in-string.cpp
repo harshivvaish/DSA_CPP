cd04802a5d963e3e461ca01d7bcd50abb29bed67
harshivvaish <harshiv.vaish25@gmail.com>
2026-06-23 18:42:11 +0530

    Time: 1 ms (73.88%) | Memory: 10 MB (46.19%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    bool is_having_same_frequency(int a[], int b[]){
-        for(int i=0; i<26; i++){
-            if(a[i] != b[i]) return false;
-        }
-        return true;
-    }
+    
     bool checkInclusion(string s1, string s2) {
         int freq[26]={0};
-        int window_size=s1.length();
+        int window_freq[26]={0};
+        int wind_size=s1.length();
         int n2=s2.length();
-        for(int i=0; i<window_size; i++){
+        if(wind_size>n2) return false;
+        for(int i=0; i<wind_size; i++){
             freq[s1[i]-'a']++;
+            window_freq[s2[i]-'a']++;
         }
+        if(equal(begin(freq), end(freq), begin(window_freq))) return true;
 
-        for(int i=0; i<n2; i++){
-            int window_st=0, idx=i;
-            int window_freq[26]={0};
+        for(int i=1; i<=n2-wind_size; i++){
+            window_freq[s2[i-1]-'a']--;
+            window_freq[s2[i+wind_size-1]-'a']++;
 
-            while(window_st<window_size && idx<n2){
-                window_freq[s2[idx]-'a']++;
-                window_st++;
-                idx++;
-            }
-            if(is_having_same_frequency(window_freq, freq)){
-                return true;
-            } 
+            if(equal(begin(freq), end(freq), begin(window_freq))) return true;
         }
         return false;
     }
