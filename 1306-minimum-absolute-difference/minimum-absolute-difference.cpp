ee428608777191140a83302116559556926af896
harshivvaish <harshiv.vaish25@gmail.com>
2026-06-24 14:46:36 +0530

    Time: 16 ms (48.26%) | Memory: 36.7 MB (46.53%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -1,21 +1,43 @@
+// class Solution {
+// public:
+//     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+//         sort(arr.begin(), arr.end());
+//         vector<vector<int>> ans;
+//         int min_diff=INT_MAX;
+        
+//         for(int i=0; i<=arr.size()-2; i++){
+//             int temp_min_diff = arr[i+1]-arr[i];
+//             if(temp_min_diff<min_diff){
+//                 ans.clear();
+//                 min_diff=temp_min_diff;
+//                 ans.push_back({arr[i], arr[i+1]});
+//             }
+//             else if(temp_min_diff==min_diff){
+//                 ans.push_back({arr[i], arr[i+1]});
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
         sort(arr.begin(), arr.end());
-        vector<vector<int>> ans;
+        int n= arr.size();
         int min_diff=INT_MAX;
-        
-        for(int i=0; i<=arr.size()-2; i++){
-            int temp_min_diff = arr[i+1]-arr[i];
-            if(temp_min_diff<min_diff){
-                ans.clear();
-                min_diff=temp_min_diff;
-                ans.push_back({arr[i], arr[i+1]});
+        for(int i=1; i<n; i++){
+            int temp = arr[i]-arr[i-1];
+            if(temp<min_diff){
+                min_diff=temp;
             }
-            else if(temp_min_diff==min_diff){
-                ans.push_back({arr[i], arr[i+1]});
+        }
+        vector<vector<int>> ans;
+        for(int i=1; i<n; i++){
+            if(arr[i]-arr[i-1]==min_diff){
+                ans.push_back({arr[i-1], arr[i]});
             }
         }
         return ans;
     }
-};
+};    
