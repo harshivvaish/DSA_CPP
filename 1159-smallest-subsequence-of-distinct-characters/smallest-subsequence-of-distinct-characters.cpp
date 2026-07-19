983ba32b076b1680fc44c57697c9bbef24d644d4
harshivvaish <harshiv.vaish25@gmail.com>
2026-07-19 11:46:25 +0530

    Time: 2 ms (17.88%) | Memory: 8.5 MB (94.04%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        int count[26] = {0};
+        bool visited[26] = {false};
+
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+
+        string result = "";
+        for (char c : s) {
+
+            count[c - 'a']--;
+
+            if (visited[c - 'a']) {
+                continue;
+            }
+            while (!result.empty() && result.back() > c && count[result.back() - 'a'] > 0) {
+                visited[result.back() - 'a'] = false;
+                result.pop_back();
+            }
+
+            result.push_back(c);
+            visited[c - 'a'] = true;
+        }
+
+        return result;
+    }
+};
